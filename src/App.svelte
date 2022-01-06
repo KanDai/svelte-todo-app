1caf4da2260e39982f1aec603cfc82b1d7bed863
Daichi Kanke <KanDai@users.noreply.github.com>
2022-01-06 18:00:43 +0900

    add: ローカルストレージ連携機能

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
-	import Task from './Task.svelte';
+	import Task from './Task.svelte'
 
 	let value = ''
-	let tasks = []
-	let lastId = tasks.length ? tasks[tasks.length - 1].id : 0
+	const idKey = 'svelteTodoAppId'
+	const taskKey = 'svelteTodoAppTasks'
+	const tempSavedTasks = localStorage.getItem(taskKey)
+    let tasks = tempSavedTasks ? JSON.parse(tempSavedTasks) : []
+	let lastId = localStorage.getItem(idKey)
+		? Number(localStorage.getItem(idKey))
+		: 0
 
 	const addTask = () => {
 		lastId += 1
@@ -12,14 +17,20 @@
 			label: value
 		}
 		tasks = [...tasks, newTask]
+		localStorage.setItem(idKey, lastId)
+		localStorage.setItem(taskKey, JSON.stringify(tasks))
 		value = ''
 	}
 
-	const handleEdit = (ev) => tasks[ev.detail.index].label = ev.detail.label
+	const handleEdit = (ev) => {
+		tasks[ev.detail.index].label = ev.detail.label
+		localStorage.setItem(taskKey, JSON.stringify(tasks))
+	}
 
 	const handleDelete = (ev) => {
 		tasks.splice(ev.detail.index, 1)
 		tasks = tasks
+		localStorage.setItem(taskKey, JSON.stringify(tasks))
 	}
 </script>
 
